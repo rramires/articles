15dc1a42b3a7459f2b68bd1415850e205acfe03f
rramires <ricardo@flexbr.com>
2023-09-08 18:30:59 -0300

    API NodeExpress add Cors, Morgan e Helmet

--- NodeExpressAPI/index.js
+++ NodeExpressAPI/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const app = express();
 
+// middlewares
+app.use(cors(/* 
+              { origin: 'http://site.abc',
+                optionsSuccessStatus: 200 } 
+             */));
+app.use(morgan('dev'));
+app.use(helmet());
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello, world!');
 });
 
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+  console.log('Server listening on port 3000');
 });
